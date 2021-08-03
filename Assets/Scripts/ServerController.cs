364f53da8b155a473e28a68c59be080919488a15
Andrew <asnow4u@gmail.com>
2021-08-02 19:30:10 -0600

    Adding Assets

---
+++ Assets/Scripts/ServerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ServerController : MonoBehaviour
+{
+
+    void Awake()
+    {
+      PhotonNetwork.ConnectUsingSettings("v1.0");
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnConnectedToMaster()
+    {
+      PhotonNetwork.JoinLobby();
+      Debug.Log("Joining Lobby"); //GOOD
+    }
+
+    public void OnJoinedLobby() {
+      SceneManager.LoadScene("Lobby");
+      Debug.Log("Joined Lobby"); //GOOD
+    }
+}
